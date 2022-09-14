15ca6a48f09bec038aa3574e4201e48c800efaa0
matias <youlost.tute@gmail.com>
2022-09-14 20:11:31 -0300

    Agrega funcionamiento Disparar

---
+++ Personaje/Disparar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparar : MonoBehaviour
+{
+    public GameObject projectile;
+    public float launchVelocity = 700f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject ball = Instantiate(projectile, transform.position,
+                                                      transform.rotation);
+            
+            ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, launchVelocity));
+        }
+    }
+}
