b0cc1e849b1ad7d6c28db08f2c067e446b0113dd
matias <youlost.tute@gmail.com>
2022-09-16 11:39:30 -0300

    agrega componente vision(VACIO) y agrega version random de seleccion de target

--- Assets/Scrips-Unity/Enemigo/MovimientoALT.cs
+++ Assets/Scrips-Unity/Enemigo/MovimientoALT.cs
@@ -1,36 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class MovimientoALT : MonoBehaviour
 {
     public Transform[] destinos;
+    public bool destinosRandom;
     private int numeroDestino = 0;
     private NavMeshAgent agent;
-
+    
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
 
+        agent.speed = 3f;
         agent.autoBraking = false;
 
         irASiguientePosicion();
     }
 
 
-    void irASiguientePosicion()
-    {
-        if (destinos.Length == 0)
-            return;
 
-        agent.destination = destinos[numeroDestino].position;
-
-        numeroDestino = (numeroDestino + 1) % destinos.Length;
-    }
 
     void Update()
     {
+        if (destinosRandom == true && estaCercaDestino())
+        {
+            if (destinos.Length == 0)
+                return;
+
+            numeroDestino = Random.Range(0, destinos.Length);
+        }
 
         if (estaCercaDestino())
             irASiguientePosicion();
@@ -39,6 +41,16 @@ public class MovimientoALT : MonoBehaviour
     private bool estaCercaDestino()
     {
         return agent.remainingDistance < 0.5f;
+    }
+
+    private void irASiguientePosicion()
+    {
+        if (destinos.Length == 0)
+            return;
+
+        agent.destination = destinos[numeroDestino].position;
+
+        numeroDestino = (numeroDestino + 1) % destinos.Length;
     }
 }
 
